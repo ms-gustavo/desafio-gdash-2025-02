0f31ddfd9f29f7e029dae503f3ed6133b8f1d1bf
Gustavo Maia <ms.gustavo@outlook.com>
2025-11-21 20:31:55 -0300

    feat: Adicionar WeatherController e módulo de clima

--- backend/api/src/weather/weather.module.ts
+++ backend/api/src/weather/weather.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { WeatherController } from './weather.controller';
 
-@Module({})
+@Module({
+  controllers: [WeatherController]
+})
 export class WeatherModule {}
